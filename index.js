8fcdf6c43b918f88cf9bb57c6487e42b29645d30
Manuel Blanco <manuel.blanco77@gmail.com>
2020-10-07 09:35:26 -0300

    Configurando express

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
+const { response } = require('express');
+const express = require('express');
 
+//crear el servidor de express
 
-console.log('hola mundo');
+const app = express();
+
+//escuchar peticiones
+
+app.listen( 4000, () => {
+    console.log(`Servidor corriendo en puerto ${ 4000 }`)
+});
+
+//rutas
+
+app.get( '/', (req, res) => {
+
+    res.json({
+        ok: true
+    });
+
+});
