4f812c589a09e3595b67c9ebbf820d40feda11e6
Manuel Blanco <manuel.blanco77@gmail.com>
2020-10-08 16:12:50 -0300

    Subiendo custom middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -5,16 +5,6 @@ const crearUsuario = ( req, res = response ) => {
 
     const { name, email, password } = req.body;
 
-    //manejo de errores
-    const errors = validationResult(req);
-    if ( !errors.isEmpty() ) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        });
-    } 
-
-
     res.status(201).json({
         ok: true,
         msg: 'registro',
@@ -26,14 +16,6 @@ const crearUsuario = ( req, res = response ) => {
 }
 
 const loginUsuario = ( req, res = response ) => {
-
-    const errors = validationResult(req);
-    if ( !errors.isEmpty() ) {
-        return res.status(400).json({
-            ok: false,
-            errors: errors.mapped()
-        });
-    } 
 
     const { email, password } = req.body;
 
