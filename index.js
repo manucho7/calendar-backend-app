3c6e7452284e7ee94fd1c6593477e81456165faa
Manuel Blanco <manuel.blanco77@gmail.com>
2020-10-09 17:40:35 -0300

    Conectando node a mongodb atlas

--- index.js
+++ index.js
@@ -1,23 +1,31 @@
 const express = require('express');
 require('dotenv').config();
+const { dbConnection } = require('./database/config');
 
 //crear el servidor de express
-
 const app = express();
 
-//escuchar peticiones
+//Base de Datos DB
+dbConnection();
 
-app.listen( process.env.PORT, () => {
-    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`)
-});
 
 //Directorio Publico middleware para configurar acceso a carpeta publica
 app.use( express.static('public') );
-
 
 // Lectura y parseo del body
 app.use( express.json() );
 
 //rutas
-
 app.use( '/api/auth', require('./routes/auth') );
+
+
+//escuchar peticiones
+app.listen( process.env.PORT, () => {
+    console.log(`-Servidor corriendo en puerto ${ process.env.PORT }`)
+});
+
+
+
+
+
+
