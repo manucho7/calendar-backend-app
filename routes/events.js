ffdd9b2021e8920c305921bc7fb57d90b790af02
Manuel Blanco <manuel.blanco77@gmail.com>
2020-10-13 09:13:54 -0300

    Validando campos necesarios en ruta para crear eventos

--- routes/events.js
+++ routes/events.js
@@ -6,6 +6,9 @@
 const { Router } = require('express');
 const { validarJWT } = require('../middlewares/validar-jwt');
 const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
+const { check } = require('express-validator');
+const { validarCampos } = require('../middlewares/validar-campos');
+const { isDate } = require('../helpers/isDate');
 
 const router = Router();
 
@@ -17,7 +20,16 @@ router.use( validarJWT );
 router.get('/', getEventos);
 
 //Crear un nuevo evento
-router.post('/', crearEvento);
+router.post(
+    '/',
+    [
+        check('title', 'El titulo es obligatorio').not().isEmpty(),
+        check('start', 'Fecha de inicio es obligatoria').custom( isDate ),
+        check('end', 'Fecha de finalizacion es obligatoria').custom( isDate ),
+        validarCampos
+    ],
+    crearEvento
+);
 
 //Actualizar Evento
 router.put('/:id', actualizarEvento);
