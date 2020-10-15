33f0a3a58dfe84f6fd001db68b2089151292493e
Manuel Blanco <manuel.blanco77@gmail.com>
2020-10-15 14:44:04 -0300

    Obtener el listado de los eventos, getEvents

--- controllers/events.js
+++ controllers/events.js
@@ -3,10 +3,14 @@ const { response } = require('express');
 const Evento = require('../models/Evento');
 
 
-const getEventos = ( req, res=response ) => {
+const getEventos = async ( req, res=response ) => {
+
+    const eventos = await Evento.find()
+                                .populate('user', 'name');
+
     res.json({
         ok: true,
-        msg: 'getEventos'
+        eventos
     })
 }
 
