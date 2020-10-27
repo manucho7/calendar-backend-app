bcdb7c315827aa1cac0b28dcebd9de6b01b3068e
Manuel Blanco <manuel.blanco77@gmail.com>
2020-10-27 13:03:00 -0300

    Version 1 del calendarApp

---
+++ public/precache-manifest.d1127b470a9994678f0880be3a8ce98f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee3072d8bd7a0527b616f681d28fa744",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d91d2ae0619ee8e94d3",
+    "url": "/static/css/2.ceee16bc.chunk.css"
+  },
+  {
+    "revision": "2cf1e0d1dc3d9fe9c369",
+    "url": "/static/css/main.dadd6b22.chunk.css"
+  },
+  {
+    "revision": "5d91d2ae0619ee8e94d3",
+    "url": "/static/js/2.1ac6a198.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "/static/js/2.1ac6a198.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cf1e0d1dc3d9fe9c369",
+    "url": "/static/js/main.69099531.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
